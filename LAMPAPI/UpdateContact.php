1eef4e1562b87775d8ecf369e089b430e906a1d2
ChristianSilva1 <59715321+ChristianSilva1@users.noreply.github.com>
2021-09-23 09:25:42 -0400

    Update UpdateContact.php

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -4,7 +4,7 @@ ini_set("error_log", "/tmp/php.log");
 error_reporting(E_ALL);
 $inData = getRequestInfo();
 
-    $UserId = $inData["id"];
+  $contactId = $inData["id"];
 	$ContactFirstName = $inData["addFirstName"];
 	$ContactLastName = $inData["addLastName"];
 	$Email = $inData["addEmail"];
@@ -17,11 +17,12 @@ $inData = getRequestInfo();
 	}
 	else
 	{
+
 			$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ?, LastName = ?, Email = ?, Phone = ? WHERE id = ?");
-			$stmt->bind_param("ssssi", $ContactFirstName, $ContactLastName, $Email, $Phone, $UserId);
-		    $stmt->execute();
+			$stmt->bind_param("sssss", $ContactFirstName, $ContactLastName, $Email, $Phone, $contactId);
+		  	$stmt->execute();
 			$result = $stmt->get_result();
-			returnWithError($UserId);
+			returnWithError($contactId);
 
 			$stmt->close();
 			$conn->close();
@@ -37,13 +38,13 @@ $inData = getRequestInfo();
 		header('Content-type: application/json');
 		echo $obj;
 	}
-	
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
-	
+
 	function returnWithInfo( $searchResults )
 	{
 		$retValue = '{"results":[' . $searchResults . '],"error":""}';
